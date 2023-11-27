14c23c0bb4150b5ee3ab51125405b9e44f7644d5
Aiden6767 <143815700+Aiden6767@users.noreply.github.com>
2023-11-27 08:44:52 -0600

    sync

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,1 +1,9 @@
 var reset = document.getElementById(reset).value = reset;
+
+var b = remainder;
+var p = principal;
+var r = interest, rate;
+var t = number, of, years;
+var m = monthly, payment, amount;
+var n = 12;
+
