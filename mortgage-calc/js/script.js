39e3c7e4cb1603929d896545002d1983c471d794
Picleo <143815700+Aiden6767@users.noreply.github.com>
2023-11-28 08:00:37 -0600

    sync

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,9 +1,32 @@
+var output = "";
+
+var I = 0.0575;
+var LoanAmount = parseFloat(prompt("Enter the loan amount:",0));
+var NumOfYears = parseInt(prompt("Enter the term years:",0));
+
+var MortgageLoanBalance;
+var MonthlyPayment;
+
+MonthlyPayment = (((I / 12) * LoanAmount) / (1- (Math.pow (1 + (I / 12), (NumOfYears * -12))))).toFixed(2);
+var TotalInterestAmount = MonthlyPayment * (NumOfYears * 12) - LoanAmount;
+var TotalLoan = LoanAmount + TotalInterestAmount;
+MortgageLoanBalance = TotalLoan - MonthlyPayment;
+
+if (MortgageLoanBalance > 0 ) {
+    document.write("Your " + NumOfYears + " year mortgage amount is " + MonthlyPayment + " per month." );
+
+    document.write(TotalInterestAmount);
+    document.write(TotalLoan);
+    document.write(MortgageLoanBalance);
+
+    for (i = 0; i <= MonthlyPayment; i++) {
+        document.write("Month " + i + ": Your Monthly Payment " + MonthlyPayment + " and Mortgage loan balance " + MortgageLoanBalance + "<br>");
+    }
+} else {
+    document.write("You have no payments");  
+}
 var reset = document.getElementById(reset).value = reset;
 
-var b = remainder;
-var p = principal;
-var r = interest, rate;
-var t = number, of, years;
-var m = monthly, payment, amount;
-var n = 12;
+document.getElementById("out").innerHTML = output;
+
 
