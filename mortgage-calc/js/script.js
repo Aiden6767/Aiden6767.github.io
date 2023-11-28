f28dbee3f5a6a18a51f80500dc71a73487c0c645
Picleo <143815700+Aiden6767@users.noreply.github.com>
2023-11-28 12:56:07 -0600

    xx

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,13 +1,15 @@
 var output = "";
 
 function mortgage() {
-  let b = remainingloanbalance();
-  let p = principal();
-  let r = interestrate();
-  let m = monthlypaymentamount();
-  let n = 12();
-
-  var loan_balance = calculate_balance(monthly_payment, interest_rate / 12, term_remaining * 12 )}
+  var b = remainingloanbalance();
+  var p = principal();
+  var r = interestrate();
+  var m = monthlypaymentamount();
+  var n = 12();
+  while (remainingloanbalance !='0') {
+    var loan_balance = calculate_balance(monthly_payment, interest_rate / 12, term_remaining * 12 )
+  } 
+}
   alert ("remaining" + loan_balance);
      
      
