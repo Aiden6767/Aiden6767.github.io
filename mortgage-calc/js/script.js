a8b1e9c453e0fb717f4304a965b1978e2fbaa1c2
Picleo <143815700+Aiden6767@users.noreply.github.com>
2023-11-28 12:49:05 -0600

    jj

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -5,7 +5,7 @@ function mortgage() {
   let p = principal();
   let r = interestrate();
   let m = monthlypaymentamount();
-  let n = 12;
+  let n = 12();
 
   var loan_balance = calculate_balance(monthly_payment, interest_rate / 12, term_remaining * 12 )}
   alert ("remaining" + loan_balance);
