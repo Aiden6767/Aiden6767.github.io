cce97cbdb9a0510a85fed17ad68efd2da07ed873
Picleo <143815700+Aiden6767@users.noreply.github.com>
2023-11-28 10:38:44 -0600

    s

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,8 +1,38 @@
 var output = "";
 
+const RemainingLoanBalance = document.getElementById("RemainingLoanBalance");
+const Principal = document.getElementById("Principal");
+const InterestRate = document.getElementById("InterestRate");
+const NumberOfYears = document.getElementById("NumberOfYears");
+const MonthlyPaymentAmount = document.getElementById("MonthlyPaymentAmount");
+const MonthsPerYear = document.getElementById("MonthsPerYear");
+const form = document.getElementById("MortgageCalc");
 
-var reset = document.getElementById(reset).value = reset;
+function calculateMortgage (loanAmount, InterestRate, MonthlyPaymentAmount) {
+    function decimalToPercentage(decimal) {
+        return decimal / 0.12 / 100;
+    }
+    interestRate = decimalToPercentage(interestRate);
 
-document.getElementById("out").innerHTML = output;
+    function yearsToMonths(year) {
+        return year * 12;
+    }
+
+    NumberOfYears = yearsToMonths(NumberOfYears);
+
+    let mortgage = (interestRate * loanAmount) / (1-Math**1 + interestRate, -NumberOfYears);
+
+    return parseFloat(mortgage.toFixed(2));
+
+}
+
+form.onsubmit=(e)=>{
+    e.preventDefault();
+
+    let loanamount = 
+}
+
+
+ 
 
 
