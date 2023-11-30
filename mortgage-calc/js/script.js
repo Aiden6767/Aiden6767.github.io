b8756a6d5879f3ff68376bff279db0874ac6d487
Picleo <143815700+Aiden6767@users.noreply.github.com>
2023-11-29 19:15:51 -0600

    sync

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -5,3 +5,20 @@ function reset() {
   document.getElementById("monp").innerHTML = "";
   document.getElementById("out").innerHTML = "";
 }
+function getRemainder() {
+  var remainder = principal (1 + int/12)**12,noy; - monp [(1 + int/12)**12,noy - 1] / (1 + int/12);
+
+}
+function calc() {
+  var remainder = Number(document.getElementById("rem").value);
+  var principal = Number(document.getElementById("prin").value);
+  var interestrate = Number(document.getElementById("int").value);
+  var remainder = principal (1 + int/12)**12,noy; - monp [(1 + int/12)**12,noy - 1] / (1 + int/12);
+  document.getElementById("out").innerHTML = "Remainder: " + remainder + "<br/>interestrate: " + int;
+}
+var n = 12;
+var monp = m;
+var noy = t;
+var int = r;
+var prin = p;
+var remainder = b;
