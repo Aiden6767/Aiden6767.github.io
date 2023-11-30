a9de4f4a73836441e8a246869765676ce4ff62dc
Picleo <143815700+Aiden6767@users.noreply.github.com>
2023-11-30 17:58:54 -0600

    sync

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,20 +1,20 @@
 function reset() {
   document.getElementById("prin").value = "";
   document.getElementById("int").value = "";
-  document.getElementById("noy").innerHTML = "";
-  document.getElementById("monp").innerHTML = "";
-  document.getElementById("nom").innerHTML = "";
+  document.getElementById("noy").value = "";
+  document.getElementById("monp").value = "";
+  document.getElementById("nom").value = "";
   document.getElementById("out").innerHTML = "";
 }
 function getRemainder() {
-  var remainder = principal (1 + int/12)**12,noy; - monp [(1 + int/12)**12,noy - 1] / (1 + int/12);
+  var b = p (1 + r/12)**12 / t - m (1 + r/12)**12 / t - 1 / (1 + r/12);
 
 }
 function calc() {
   var remainder = Number(document.getElementById("rem").value);
   var principal = Number(document.getElementById("prin").value);
   var interestrate = Number(document.getElementById("int").value);
-  var b = p * (1 + r/12)**12 * t - m * (1 + r/12)**12 * t - 1 / (1 + r/12);
+  var b = p * (1 + r/12)**12 / t - m * (1 + r/12)**12 / t - 1 / (1 + r/12);
   document.getElementById("out").innerHTML = "Remainder: " + remainder + "<br/>interestrate: " + r;
 }
 var nom = 12;
