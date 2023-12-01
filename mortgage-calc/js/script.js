c3335b1ce57baa013543c08a49488c2e68e3cabc
Aiden6767 <143815700+Aiden6767@users.noreply.github.com>
2023-12-01 08:33:19 -0600

    sync

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,16 +1,17 @@
 function reset() {
-var M = Number(document.getElementById("M").value);
-var N = Number(document.getElementById("N ").value);
-var P = Number(document.getElementById("P").value);
-var O = Number(document.getElementById("O").value);
-var R = Number(document.getElementById("R").value);
+var M = Number(document.getElementById("M").value = "");
+var P = Number(document.getElementById("P").value = "");
+var N = Number(document.getElementById("N").value = "");
+var R = Number(document.getElementById("R").value = "");
+var T = Number(document.getElementById("T").value = "");
 }
 function calc() {
-  var M = Number(document.getElementById("rem").value);
-  var P = Number(document.getElementById("prin").value);
-  var N = Number(document.getElementById("int").value);
-  var O = 12;
-  var B = (P*(1 + (R/12))**(12*T)) - ((M*((1 + (R/12))**(12*T)-1))/(R/12));
+  var M = Number(document.getElementById("M").value);
+  var P = Number(document.getElementById("P").value);
+  var R = Number(document.getElementById("R").value);
+  var T = Number(document.getElementById("T").value);
+  var N = 12;
+  var B = (P*(1 + (R/N))**(N*T)) - ((M*((1 + (R/N))**(N*T)-1))/(R/N));
   var out = document.getElementById("out");
   out.innerText="$" + B.toFixed(2);
 }
