93e01b12f4a0be8fd40d048122af4485dc5cff76
Picleo <143815700+Aiden6767@users.noreply.github.com>
2023-11-30 21:19:21 -0600

    sync

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,24 +1,17 @@
 function reset() {
-  document.getElementById("12").value = "";
-  document.getElementById("prin").value = "";
-  document.getElementById("int").value = "";
-  document.getElementById("noy").value = "";
-  document.getElementById("monp").value = "";
-  document.getElementById("out").innerHTML = "";
+var M = Number(document.getElementById("M").value);
+var N = Number(document.getElementById("N ").value);
+var P = Number(document.getElementById("P").value);
+var O = Number(document.getElementById("O").value);
+var R = Number(document.getElementById("R").value);
 }
 function calc() {
-  var r = Number(document.getElementById("rem").value);
-  var p = Number(document.getElementById("prin").value);
-  var t = Number(document.getElementById("int").value);
-  var t = Number(document.getElementById("int").value);
-  var n = 12;
-  var b = (p*(1 + (r/12))**(12*t)) - ((m*((1 + (r/12))**(12*t)-1))/(r/12));
+  var M = Number(document.getElementById("rem").value);
+  var P = Number(document.getElementById("prin").value);
+  var N = Number(document.getElementById("int").value);
+  var O = 12;
+  var B = (p*(1 + (r/12))**(12*t)) - ((m*((1 + (r/12))**(12*t)-1))/(r/12));
   var out = document.getElementById("out");
   out.innerText="$" + b.toFixed(2);
 }
-var n = 12;
-var monp = m;
-var noy = t;
-var int = r;
-var prin = p;
-var remainder = b;
+
