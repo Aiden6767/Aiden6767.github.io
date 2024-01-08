02f5ed6f2234835e16b1006e7ab9a869c5af2959
Aiden6767 <143815700+Aiden6767@users.noreply.github.com>
2024-01-08 08:45:01 -0600

    sync

--- js/script.js
+++ js/script.js
@@ -4,26 +4,10 @@ function updateTime() {
     var m = ("0" + date.getMinutes()).slice(-2);
     var h = ("0" + date.getHours()).slice(-2);
     var clr = "#" + h + m + s;
-    document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
-    document.getElementById("bdy").styles.backgroundColor = clr;
-    document.getElementById("hex").innerHTML = clr;
+    document.getElementById("clock").innerText = h + ":" + m + ":" + s;
 }
 setInterval(updateTime, 1000);
-$("h2").each(function(index, element){
-    var animation = TweenMax.to(this, 0.2, {
-      className: '+= superShadow',
-      marginTop: '-10px',
-      marginBottom: '10px',
-      ease: Power1.easeIn,
-      paused:true
-    });
-    element.animation = animation;
-  })
-  $('h2').hover(function(){
-   this.animation.play()
-  }, function(){
-   this.animation.reverse();
-  })
+
   function color(z) {
     var a = getComputedStyle(z);
     var b = a.backgroundColor;
@@ -46,6 +30,6 @@ function next(){
 }
 
 function setImg(){
-	return slider-img.setAttribute('src', "image/"+images[i]);
+	return slider-img.setAttribute("src", "image/"+images[i]);
 
 }
